4010ec3916538655c5c57f40ad661141273f6080
Glen Shaji <glenkshaji@gmail.com>
2021-09-06 17:45:28 +0300

    new user connections

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,18 @@
-import express from 'express';
+import express, { Application, Request, Response } from 'express';
+import { Server } from 'socket.io';
 
-const app = express();
+const app: Application = express();
+const PORT = 5000 || process.env.PORT;
+const expressServer = app.listen(PORT, () => {
+  console.log(`Server running on PORT ${PORT}`);
+});
+const io = new Server(expressServer);
 
-const PORT = 5000;
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello');
+});
 
-app.listen(PORT, () => {
-  console.log(`Server running on ${PORT}`);
+io.on('connection', (socket: any) => {
+  // this message can be logged once we connect to the client-side
+  console.log(`User connected with id ${socket.id}`);
 });
---
+++ server/utils/config.ts
@@ -0,0 +1,5 @@
+const Config = {
+  serverPort: process.env.PORT || 5000,
+};
+
+export default Config;
