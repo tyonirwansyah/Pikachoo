c0db3d3f703b68ef80430894c939fe0ea3218dd2
Glen Shaji <glenkshaji@gmail.com>
2021-09-15 18:30:11 +0300

    modified property type to objectid

--- server/src/models/Group.ts
+++ server/src/models/Group.ts
@@ -1,24 +1,31 @@
-import {
-  getModelForClass,
-  index,
-  modelOptions,
-  prop,
-} from '@typegoose/typegoose';
+import { getModelForClass, modelOptions, prop } from '@typegoose/typegoose';
 import { UserSchema } from './User';
+import mongoose from 'mongoose';
 
-@modelOptions({ options: { allowMixed: 1 } })
-@index({ unique: 1, _id: 1 })
-class Group {
+@modelOptions({
+  schemaOptions: {
+    collection: 'group',
+  },
+})
+class GroupSchema {
   @prop({ required: true, type: String })
   public name?: string;
 
-  @prop({ required: true, type: UserSchema })
+  @prop({
+    required: true,
+    type: mongoose.Types.ObjectId,
+    ref: () => UserSchema,
+  })
   public owner?: UserSchema;
 
-  @prop({ required: true, type: [UserSchema], default: [] })
+  @prop({
+    required: true,
+    type: mongoose.Types.ObjectId,
+    default: [],
+    ref: () => UserSchema,
+  })
   public members?: UserSchema[];
 }
 
-const GroupModel = getModelForClass(Group);
-
+const GroupModel = getModelForClass(GroupSchema);
 export default GroupModel;
